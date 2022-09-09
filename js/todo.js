5d9a369c14a010d2c5bf1f81257869943bd05026
serfiraz-j <75641519+serfiraz-j@users.noreply.github.com>
2022-09-09 08:34:23 +0300

    patika-js-odev2ToDo

---
+++ js/todo.js
@@ -0,0 +1,63 @@
+let taskList = JSON.parse(localStorage.getItem('taskList'))
+let ulList = document.querySelector('#list') //tasklarımızın parent elemanı olan ul yi id si ile aldık
+
+// task üstüne tıklanmasını yakalama
+ulList.addEventListener('click', function(e) {
+    if (e.target.tagName === 'LI') {
+        e.target.classList.toggle('checked');
+    }
+})
+
+showTaskList() //listemizi çağırdık
+
+// task ekleme
+function newElement() {
+    let newTask = document.querySelector('#task').value.trim() //trim fonksiyonu başındaki sonundaki boşlukları siler
+    if (localStorage.getItem('taskList') === null) {
+        taskList = [];
+    } else {
+        taskList = JSON.parse(localStorage.getItem('taskList')) //storage'tan json ile çekip veriyi array olarak almış oluyoruz
+    }
+    if (newTask !== '') {
+        taskList.push(newTask)
+        showTaskList()
+        $(".success").toast("show")
+
+    } else {
+        $(".error").toast("show")
+    }
+    localStorage.setItem('taskList', JSON.stringify(taskList)) //stroge'ı güncelliyoruz json stringfy ile tekrar string olarak yolluyoruz
+
+}
+
+// task silme 
+function removeTask() {
+    this.parentElement.remove(); //listeden sil
+
+    taskList.splice(this.id, 1) //local storage güncellemesi için önce arraydan siliyoruz
+    localStorage.setItem('taskList', JSON.stringify(taskList)) //local storage'tan da silmiş olduk
+    showTaskList()
+
+
+}
+
+// task listesini çağırma
+function showTaskList() {
+    if (taskList) {
+        ulList.innerHTML = ''
+        taskList.forEach((task, index) => {
+            let yeniLi = document.createElement('li')
+            yeniLi.textContent = task
+            const span = document.createElement("span")
+            const text = document.createTextNode("\u00D7")
+            span.className = "close"
+            span.id = index
+            span.onclick = removeTask
+            span.appendChild(text)
+            yeniLi.appendChild(span)
+            ulList.appendChild(yeniLi)
+
+        });
+
+    }
+}
